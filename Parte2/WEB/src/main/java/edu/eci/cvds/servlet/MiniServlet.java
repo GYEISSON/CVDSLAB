3d9e60ae41d6e48f5fcc0cdb6154546bc99bf72f
Carlos <manuel.murillo.1999@gmail.com>
2019-09-16 22:51:29 -0500

    Correccion parte 2 punto 13

--- Parte2/WEB/src/main/java/edu/eci/cvds/servlet/MiniServlet.java
+++ Parte2/WEB/src/main/java/edu/eci/cvds/servlet/MiniServlet.java
@@ -17,10 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 import edu.eci.cvds.servlet.model.Todo;
 import java.io.FileNotFoundException;
 import java.net.MalformedURLException;
+import java.util.*;
 
 /**
  *
- * @author 2152972
+ * @author Carlos Murillo - Yeisson Gualdron
  */
 
 @WebServlet(
@@ -28,75 +29,59 @@ import java.net.MalformedURLException;
 )
 
 public class MiniServlet extends HttpServlet{
-    static final long serialVersionUID = 35L;
+	static final long serialVersionUID = 35L;
 
-    @Override
-   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-       Writer responseWriter = resp.getWriter();
-       Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
-       String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
-        
-       //resp.setStatus(HttpServletResponse.SC_OK);
-       //responseWriter.write("Hello " + id + "!");
-       //responseWriter.flush();
-       Service ser = new Service();
-       try{
-           Todo tod = ser.getTodo(Integer.parseInt(id));
-           //System.out.println(tod);
-           if( tod != null){
-            responseWriter.write("{\n");
-            resp.setStatus(HttpServletResponse.SC_OK);
-            responseWriter.write("UserId : "+tod.getUserId()+",\n");
-            responseWriter.write("id : "+tod.getId()+",\n");
-            responseWriter.write("title : "+tod.getTitle()+",\n");
-            responseWriter.write("conplete : " + tod.getCompleted()+"\n");
-            responseWriter.write("}");
-            }
-       }catch(MalformedURLException e){
-           resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-           responseWriter.write("error interno en el servidor \n");
-       }catch(FileNotFoundException f){
-           resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-           responseWriter.write("No existe un item con el identificador dado \n");
-       }
-       catch(Exception num){
-           resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-           responseWriter.write("Requerimiento invalido \n");
-       }
-   }
-   
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Writer responseWriter = resp.getWriter();
-       Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
-       String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
-        
-       //resp.setStatus(HttpServletResponse.SC_OK);
-       //responseWriter.write("Hello " + id + "!");
-       //responseWriter.flush();
-       Service ser = new Service();
-       try{
-           Todo tod = ser.getTodo(Integer.parseInt(id));
-           //System.out.println(tod);
-           if( tod != null){
-            responseWriter.write("{\n");
-            resp.setStatus(HttpServletResponse.SC_OK);
-            responseWriter.write("UserId : "+tod.getUserId()+",\n");
-            responseWriter.write("id : "+tod.getId()+",\n");
-            responseWriter.write("title : "+tod.getTitle()+",\n");
-            responseWriter.write("conplete : " + tod.getCompleted()+"\n");
-            responseWriter.write("}");
-            }
-       }catch(MalformedURLException e){
-           resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-           responseWriter.write("error interno en el servidor \n");
-       }catch(FileNotFoundException f){
-           resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-           responseWriter.write("No existe un item con el identificador dado \n");
-       }
-       catch(Exception num){
-           resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-           responseWriter.write("Requerimiento invalido \n");
-       }
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Writer responseWriter = resp.getWriter();
+		Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+		String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+		
+		try{
+			Todo tod = Service.getTodo(Integer.parseInt(id));
+			if( tod != null){
+			ArrayList<Todo> todos=new ArrayList<Todo>();
+			todos.add(tod);
+			responseWriter.write(Service.todosToHTMLTable(todos));
+			}
+		}catch(MalformedURLException e){
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			responseWriter.write("error interno en el servidor \n");
+		}catch(FileNotFoundException f){
+			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			responseWriter.write("No existe un item con el identificador dado \n");
+		}
+		catch(Exception num){
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			responseWriter.write("Requerimiento invalido \n");
+		}
+		responseWriter.flush();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Writer responseWriter = resp.getWriter();
+		Optional<String> optName = Optional.ofNullable(req.getParameter("id"));
+		String id = optName.isPresent() && !optName.get().isEmpty() ? optName.get() : "";
+		
+		try{
+			Todo tod = Service.getTodo(Integer.parseInt(id));
+			if( tod != null){
+			ArrayList<Todo> todos=new ArrayList<Todo>();
+			todos.add(tod);
+			responseWriter.write(Service.todosToHTMLTable(todos));
+			}
+		}catch(MalformedURLException e){
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			responseWriter.write("error interno en el servidor \n");
+		}catch(FileNotFoundException f){
+			resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+			responseWriter.write("No existe un item con el identificador dado \n");
+		}
+		catch(Exception num){
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			responseWriter.write("Requerimiento invalido \n");
+		}
+		responseWriter.flush();
+	}
 }
